18ff3110275591f8f49a3d774135e2e3630f0918
Sebastian <a.sebastian.uc@gmail.com>
2026-03-03 21:35:23 +0100

    Adding pokemon gallery

---
+++ src/api/pokeAPi.js
@@ -0,0 +1,56 @@
+// fetch the pokemons from the list
+async function fetchData() {
+  try {
+    // Get a list of pokemons
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon/?limit=60&offset=0",
+    );
+
+    // Show an error if couldn't fetch the data
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+    // turn the response into a json
+    const data = await response.json();
+
+    // fetch the url of the pokemons from the list
+    const urls = data.results.map((pokemon) => pokemon.url);
+
+    return urls;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Pass the sprites url
+export async function spritePrinter() {
+  try {
+    // an async func return a promise
+
+    const data = await fetchData();
+
+    // iterate the data list of url to get each pokemon data
+    const spriteUrls = data.map(async (url) => {
+      const res = await fetch(url); // fetch the pokemon url
+      const json = await res.json(); // convert to json
+      return json;
+    });
+
+    // const pokemonImageArray = await spriteUrls.json();
+
+    // Wait until all the promise are fullfilled
+    const pokemonData = await Promise.all(spriteUrls);
+
+    let urls = [];
+
+    pokemonData.map((data) => {
+      urls.push(data.sprites.front_default);
+    });
+
+    return urls;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+spritePrinter();
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { spritePrinter } from "../api/pokeAPi";
+
+function Gallery() {
+  const [sprites, setSprite] = useState([]);
+
+  useEffect(() => {
+    async function loadImages() {
+      const images = await spritePrinter();
+      setSprite(images);
+    }
+
+    loadImages();
+  }, []);
+
+  return (
+    <div className="galleryContainer">
+      <h1>Pokemon Api</h1>
+      <div className="images-grid" id="imageContainer">
+        {sprites.map((sprite, index) => (
+          <img src={sprite} key={index} alt="pokemon-image" />
+        ))}
+      </div>
+      {/* <div>{sprites}</div> */}
+    </div>
+  );
+}
+
+export default Gallery;
